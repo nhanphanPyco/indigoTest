e6458188a1ab1acc5335f28baca24f2642cce36f
Phan Dai Nhan <nhan.phan@pycogroup.com>
2019-10-01 20:02:32 +0700

    no message

--- ReactTest/App.js
+++ ReactTest/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'glamorous-native'
-import createSagaMiddleware from 'redux-saga'
 
 import Navigator from './src/Navigation/index'
 import store from './src/redux/createStore'
 import theme from './src/themes/theme'
-import rootSaga from './src/redux/rootSaga'
 
-// const sagaMiddleware = createSagaMiddleware().run(rootSaga);
 const App = () => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
@@ -16,7 +13,6 @@ const App = () => (
     </ThemeProvider>
   </Provider>
 )
-// sagaMiddleware.run(rootSaga);
 
 export default App
 
